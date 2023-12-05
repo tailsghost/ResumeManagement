32870057448e0a6aaa6aa017dad24b4814e96db8
AndreyXP <andreyapple99@mail.ru>
2023-12-05 20:30:43 +0300

    Очистка мусора с клиента и первоначальные данные

--- resumemanagement.client/src/App.tsx
+++ resumemanagement.client/src/App.tsx
@@ -1,56 +1,16 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
+import { useContext } from "react";
+import { ThemeContext } from "./context/theme.context";
+import Navbar from "./components/navbar/Navbar.component";
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+  const { darkMode } = useContext(ThemeContext);
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
+  const appStyles = darkMode ? "app dark" : "app";
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+  return <div className={appStyles}>
+    <Navbar/>
+    <div className="wrapper">Навигация</div>
+  </div>;
 }
 
 export default App;
---
+++ resumemanagement.client/src/components/navbar/Navbar.component.tsx
@@ -0,0 +1,9 @@
+import './navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div>Navbar.component</div>
+  )
+}
+
+export default Navbar;
