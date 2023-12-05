40c37fc57b25ec154e64a8990ce878e9387e3d3a
AndreyXP <andreyapple99@mail.ru>
2023-12-05 21:56:05 +0300

    Добавление навигационного меню

--- resumemanagement.client/src/components/navbar/Navbar.component.tsx
+++ resumemanagement.client/src/components/navbar/Navbar.component.tsx
@@ -1,9 +1,62 @@
-import './navbar.scss'
+import { useContext } from "react";
+
+import "./navbar.scss";
+
+import { Link } from "react-router-dom";
+import { Menu, LightMode, DarkMode } from "@mui/icons-material";
+import { ToggleButton } from "@mui/material";
+import { ThemeContext } from "../../context/theme.context";
+
+const links = [
+  {
+    href: "/",
+    label: "Главная",
+  },
+  {
+    href: "/companies",
+    label: "Компании",
+  },
+  {
+    href: "/jobs",
+    label: "Вакансии",
+  },
+  {
+    href: "/candidates",
+    label: "Кандидаты",
+  },
+];
 
 const Navbar = () => {
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+
   return (
-    <div>Navbar.component</div>
-  )
-}
+    <div className="navbar">
+      <div className="brand">
+        <span>Resume Management</span>
+      </div>
+      <div className="menu">
+        <ul>
+          {links.map((item) => (
+            <li key={item.href}>
+              <Link to={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="hamburger">
+        <Menu />
+      </div>
+      <div className="toggle">
+        <ToggleButton
+          value={"check"}
+          selected={darkMode}
+          onChange={toggleDarkMode}
+        >
+          {darkMode ? <LightMode /> : <DarkMode />}
+        </ToggleButton>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
