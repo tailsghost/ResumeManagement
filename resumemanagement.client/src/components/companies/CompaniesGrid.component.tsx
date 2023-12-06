5f32c0dd30e8b58bc40283b97d9bade047598f52
AndreyXP <andreyapple99@mail.ru>
2023-12-06 17:18:31 +0300

    Подключение компонента CompaniesGrid, создание useEffect для загрузки компаний

---
+++ resumemanagement.client/src/components/companies/CompaniesGrid.component.tsx
@@ -0,0 +1,32 @@
+import { Box } from "@mui/material";
+import { GridColDef } from "@mui/x-data-grid/models";
+import { DataGrid } from "@mui/x-data-grid";
+import moment from "moment";
+import { ICompany } from "../../types/global.typing";
+
+const column: GridColDef[] = [
+  { field: "id", headerName: "Id", width: 100 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "size", headerName: "Size", width: 150 },
+  {
+    field: "createAt",
+    headerName: "Creation Time",
+    width: 200,
+    renderCell: (params) => moment(params.row.createAt).format("YYYY-MM-DD"),
+  },
+  { field: "id", headerName: "Id", width: 100 },
+];
+
+interface ICompaniesGridProps {
+    data: ICompany[];
+}
+
+const CompaniesGrid = ({ data }: ICompaniesGridProps) => {
+  return (
+    <Box sx={{width: "100%", height: 450}} className="companies-grid">
+      <DataGrid rows={data} columns={column} getRowId={(row) => row.id} rowHeight={50}/>
+    </Box>
+  );
+};
+
+export default CompaniesGrid;
--- resumemanagement.client/src/pages/companies/Companies.page.tsx
+++ resumemanagement.client/src/pages/companies/Companies.page.tsx
@@ -1,9 +1,45 @@
-import './companies.scss'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Button, CircularProgress } from "@mui/material";
+import { Add } from "@mui/icons-material";
+
+import "./companies.scss";
+import httpModule from "../../helpers/http.module";
+import { ICompany } from "../../types/global.typing";
+import CompaniesGrid from "../../components/companies/CompaniesGrid.component";
 
 const Companies = () => {
-  return (
-    <div>Companies.page</div>
-  )
-}
+  const [companies, setCompanies] = useState<ICompany[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const redirect = useNavigate();
+
+  useEffect(() => {
+    setLoading(true);
+    httpModule
+      .get<ICompany[]>("/Company/Get")
+      .then((responce) => {
+        setCompanies(responce.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert("Error");
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return <div className="content companies">
+    <div className="heading">
+      <h2>Компании</h2>
+      <Button variant="outlined" onClick={() => redirect("/companies/add")}>
+        <Add/>
+      </Button>
+    </div>
+    {
+      loading ? <CircularProgress size={100}/> : companies.length === 0 ? <h1>Нет компаний</h1> : <CompaniesGrid data={companies}/>
+    }
+  </div>;
+};
 
 export default Companies;
